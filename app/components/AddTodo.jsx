8f0521e775af0a8aba699591777cc44958ddf7f1
jameslNeurio <james.leong@neur.io>
2017-05-03 20:42:42 -0700

    addtodo tests

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var strTodo = this.refs.todoText.value;
+
+        if (strTodo.length > 0) {
+            this.refs.todoText.value = "";
+            this.props.onAddTodo(strTodo);
+        } else {
+            this.refs.todoText.focus();
+            //if there isnt valid data, puts cursor on text automatically
+        }
+        
+    },
+    render: function() {
+        return (
+            <div>
+                <form ref="form" onSubmit={this.handleSubmit} className="countdown-form">
+                    <input type="text" ref="todoText" placeholder="What do you need to do?"/>
+                    <button className="button expanded">Add Todo</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+module.exports = AddTodo;
