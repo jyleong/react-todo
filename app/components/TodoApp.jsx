5317d327c39e579014ec8ec6979964d91f0ae14c
jameslNeurio <james.leong@neur.io>
2017-05-08 21:28:45 -0700

    todoapi file added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var TodoAPI = require('TodoAPI');
 var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
@@ -9,29 +10,11 @@ var TodoApp = React.createClass({
         return {
             showCompleted: false,
             searchText: '',
-            todos: [
-                {
-                    id: uuid(),
-                    text: 'Walk the dog',
-                    completed: false
-                },
-                {
-                    id: uuid(),
-                    text: 'Clean the yard',
-                    completed: true
-                },
-                {
-                    id: uuid(),
-                    text: 'Brush my teeth',
-                    completed: true
-                },
-                {
-                    id: uuid(),
-                    text: 'Apply to jobs',
-                    completed: false
-                }
-            ]
+            todos: TodoAPI.getTodos()
         };
+    },
+    componentDidUpdate: function() {
+        TodoAPI.setTodos(this.state.todos);
     },
     handleToggle: function(id) {
         var updatedTodos = this.state.todos.map((todo) => {
