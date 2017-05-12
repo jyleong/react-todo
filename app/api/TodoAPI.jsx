72e8c6a6450d12c50d9ad9c9b6e1de53f6822f4b
jameslNeurio <james.leong@neur.io>
2017-05-11 21:21:37 -0700

    search wirred up

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -25,8 +25,22 @@ module.exports = {
         filteredTodos = filteredTodos.filter((todo) => {
             return !todo.completed || showCompleted;
         });
+        filteredTodos = filteredTodos.filter((todo) => {
+            var todoText = todo.text.toLowerCase();
+            return searchText.length === 0 || todoText.indexOf(searchText) >= 0;
+        })
         //FIlter by searchText
-        //sort todos with non-completed first
+
+        filteredTodos.sort((a,b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        
 
         return filteredTodos;
     }
