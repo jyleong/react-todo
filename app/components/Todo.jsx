29aaaabda7e1b2d4328740fa864a2a6a003f9f22
jameslNeurio <james.leong@neur.io>
2017-05-20 20:10:17 -0700

    finish styling todo app

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,6 +4,7 @@ var moment = require('moment');
 var Todo = React.createClass({
     render: function() {
         var {id, text, completed, createdAt, completedAt} = this.props;
+        var todoClassName = completed ? 'todo todo-completed' : 'todo';
         var renderDate = () => {
             var message = 'Created ';
             var timestamp = createdAt;
@@ -12,18 +13,22 @@ var Todo = React.createClass({
                 message = 'Completed ';
                 timestamp = completedAt;
             }
-            
+
             return message + moment.unix(timestamp).format('MMM Do, YYYY @  h:mm a');
         };
 
         //can define arrow funciton inside for click handler
         return (
-            <div onClick={()=> {
+            <div className={todoClassName} onClick={()=> {
                 this.props.onToggle(id);
             }}>
-                <input type="checkbox" checked={completed}/>
-                <p>{text}</p>
-                <p>{renderDate()}</p>
+                <div>
+                    <input type="checkbox" checked={completed}/>
+                </div>
+                <div>
+                    <p>{text}</p>
+                    <p className="todo-subtext">{renderDate()}</p>
+                </div>
             </div>
         );
     }
