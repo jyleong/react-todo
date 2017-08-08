4157d98d2a15289d452cd9cc681aeaebb070f37f
James Leong <jyleong@sfu.ca>
2017-08-07 17:20:55 -0700

    reducers

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -46,6 +46,11 @@ export var todosReducer = (state = [], action) => {
                 }
                 
             });
+        case 'ADD_TODOS':
+            return [
+                ...state,
+                ...action.todos
+            ];
         default:
             return state;
     };
